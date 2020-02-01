189ebf4a0c69d552c53d33c4693df76e0ee065b0
hartdegen <hartdegen182@gmail.com>
2020-02-02 00:26:48 +0300

    basic gendiff logic in index-test.js

--- src/index-test.js
+++ src/index-test.js
@@ -1,24 +1,15 @@
-const before = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
-const after = {
-  timeout: 20,
-  verbose: true,
-  host: 'hexlet.io',
-};
-
-const diff = (b, a) => {
+// trying to write diff function
+export default (before, after) => {
+  const b = JSON.parse(before);
+  const a = JSON.parse(after);
   const array = Object.entries({ ...b, ...a });
+
   const reduced = array.reduce((acc, value) => {
     const [key, val] = value;
     if (a[key] === b[key]) return [...acc, `    ${[key]}: ${a[key]}`];
     if (Object.keys(b).includes(key) && val !== b[key]) return [...acc, `  + ${[key]}: ${a[key]}`, `  - ${[key]}: ${b[key]}`];
     return (!Object.keys(a).includes(key)) ? [...acc, `  - ${[key]}: ${val}`] : [...acc, `  + ${[key]}: ${val}`];
   }, []);
+
   return `{\n${reduced.join('\n')}\n}`;
 };
-
-console.log(diff(before, after));
