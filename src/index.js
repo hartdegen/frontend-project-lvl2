f6a034816fb16c4f900e1575ba0e36e3e6b9c329
hartdegen <hartdegen182@gmail.com>
2020-02-11 02:37:43 +0300

    bad logic for tree structured JSON v.2

--- src/index.js
+++ src/index.js
@@ -11,31 +11,36 @@ const parserFunc = (before, after) => {
   const b = parser(extName, before);
   const a = parser(extName, after);
   // eslint-disable-next-line no-use-before-define
-  return diff(b, a).replace(/"/g, '');
+  return `{${render(diff(b, a))}\n}`;
+};
+
+const render = (obj, count = 0, space = '   ') => {
+  const array = Object.entries(obj);
+  return array.reduce((acc, val) => {
+    if (typeof val[1] !== 'object') return `${acc}\n ${space.repeat(count)}${val[0]}: ${val[1]}`;
+    if (typeof val[1] === 'object' && count === 0) return `${acc}\n  ${val[0]}: {${render(val[1], count + 1)}\n${space} }`;
+    if (typeof val[1] === 'object') return `${acc}\n${space} ${val[0]}: {${render(val[1], count + 1)}\n${space.repeat(2)}}`;
+  }, '');
 };
 
 const diff = (b = {}, a = {}) => {
-  const array = Object.entries({ ...b, ...a });
+  const sliced = { ...b, ...a };
+  const array = Object.entries(sliced);
   console.log('ARRAY', array);
 
   const reduced = array.reduce((acc, value) => {
-    console.log('VALUE', value);
-
-    const [key, val] = value;
-    console.log('B-KEY', b[key], 'A-KEY', a[key]);
-
-    if (b[key] === a[key]) return [...acc, `    ${[key]}: ${a[key]}`];
-    if (!Object.keys(a).includes(key)) return [...acc, `  - ${[key]}: ${JSON.stringify(val)}`];
-    if (!Object.keys(b).includes(key) && Object.keys(a).includes(key)) return [...acc, `  + ${[key]}: ${JSON.stringify(val)}`];
-
-    if (typeof b[key] === 'object' && typeof a[key] === 'object') return [...acc, `    ${[key]}: ${diff(b[key], a[key])}`];
+    if (typeof value === 'string') return value;
+    const [key] = value;
     
-    console.log('OBJECT KEYS B', Object.keys(b), 'KEY', key, 'VAL', val, 'B-KEY', b[key]);
-    if (Object.keys(b).includes(key) && val !== b[key]) return [...acc, `  - ${[key]}: ${JSON.stringify(b[key])}`, `  + ${[key]}: ${JSON.stringify(a[key])}`];
-    return [...acc, `    ${[key]}: ${a[key]}`];
-  }, []);
-
-  return `{\n${reduced.join('\n')}\n}`;
+    if (b[key] === a[key]) return { ...acc, [`  ${key}`]: a[key] };
+    if (typeof b[key] === 'object' && typeof a[key] === 'object') return { ...acc, [`  ${key}`]: diff(b[key], a[key]) };
+    if (Object.keys(b).includes(key) && !Object.keys(a).includes(key)) return { ...acc, [`- ${key}`]: b[key] };
+    if (!Object.keys(b).includes(key) && Object.keys(a).includes(key)) return { ...acc, [`+ ${key}`]: a[key] };
+    if (Object.keys(b).includes(key) && Object.keys(a).includes(key) && typeof b[key] !== 'object') return { ...acc, [`- ${key}`]: b[key], [`+ ${key}`]: a[key] };
+    if (Object.keys(b).includes(key) && Object.keys(a).includes(key) && typeof a[key] !== 'object') return { ...acc, [`- ${key}`]: b[key], [`+ ${key}`]: a[key] };
+    return { ...acc };
+  }, {});
+  return reduced;
 };
 
 export default parserFunc;
