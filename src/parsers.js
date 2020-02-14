79d517a05502fd8870de43be6f20025b89281a83
hartdegen <hartdegen182@gmail.com>
2020-02-15 01:40:56 +0300

    microrefactoring of parsers.js v.2

--- src/parsers.js
+++ src/parsers.js
@@ -1,13 +1,14 @@
 import fs from 'fs';
-import process from 'process';
 import yaml from 'js-yaml';
 import ini from 'ini';
+import process from 'process';
+import path from 'path';
 
-export default (extName, path) => {
-  const fullPath = path.resolve(path, process.cwd());
+export default (extName, somePath) => {
+  const fullPath = path.resolve(process.cwd(), somePath);
   const data = fs.readFileSync(fullPath).toString();
 
-  if (extName === '.yaml') return yaml.safeLoad(fs.readFileSync(data, 'utf-8'));
-  if (extName === '.ini') return ini.parse(fs.readFileSync(data, 'utf-8'));
-  return JSON.parse(fs.readFileSync(data, 'utf-8'));
+  if (extName === '.yaml') return yaml.safeLoad(data);
+  if (extName === '.ini') return ini.parse(data);
+  return JSON.parse(data);
 };
