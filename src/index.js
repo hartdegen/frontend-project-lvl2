e7e82371a5debe2334a13d6a97df6e57c4e3584d
hartdegen <hartdegen182@gmail.com>
2020-02-16 03:02:32 +0300

    index.js refactoring

--- src/index.js
+++ src/index.js
@@ -4,29 +4,32 @@ import formatter from './formatters/formatter';
 
 const getFileExtension = (pathToFile) => path.extname(`${pathToFile}`);
 
-const startRendering = (obj, depthСount = 0, space = '  ') => Object.entries(obj).reduce((acc, value) => {
-  const [key, val] = value;
-  return typeof val !== 'object' ? `${acc}\n${space.repeat(depthСount)}${key}: ${val}`
-    : `${acc}\n${space.repeat(depthСount)}${key}: {${startRendering(val, depthСount + 1)}\n${space}${space.repeat(depthСount)}}`;
-}, '');
-
 const keysOf = (obj) => Object.keys(obj);
-const valueOf = (obj) => (typeof obj !== 'object' ? obj
-  : Object.entries(obj).reduce((someAcc, val) => ({ ...someAcc, [`  ${val[0]}`]: `${val[1]}` }), {}));
-
 // b is "before"
 // a is "after"
-const diff = (b, a) => {
+const diff = (b, a = {}) => {
   const mergedConfigs = Object.entries({ ...b, ...a });
 
   return mergedConfigs.reduce((acc, value) => {
     const [key] = value;
-    if (b[key] === a[key]) return { ...acc, [`  ${key}`]: valueOf(a[key]) };
-    if (typeof b[key] === 'object' && typeof a[key] === 'object') return { ...acc, [`  ${key}`]: diff(b[key], a[key]) };
-    if (keysOf(b).includes(key) && keysOf(a).includes(key)) return { ...acc, [`- ${key}`]: valueOf(b[key]), [`+ ${key}`]: valueOf(a[key]) };
-    return (!keysOf(b).includes(key)) ? { ...acc, [`+ ${key}`]: valueOf(a[key]) } : { ...acc, [`- ${key}`]: valueOf(b[key]) };
-  }, {});
+    if (b[key] === a[key]) return [...acc, ['unchanged', key, a[key]]];
+    if (typeof b[key] === 'object' && typeof a[key] === 'object') return [...acc, ['gottaCheckDeeper', key, diff(b[key], a[key])]];
+    if (keysOf(b).includes(key) && keysOf(a).includes(key)) return [...acc, ['changed', key, b[key], a[key]]];
+    return (!keysOf(b).includes(key)) ? [...acc, ['added', key, a[key]]] : [...acc, ['deleted', key, b[key]]];
+  }, []);
 };
+
+const getRenderResult = (arr, depthСount = 0, space = '    ') => arr.reduce((acc, value) => {
+  const [status, key, val, nextVal] = value;
+  const getValue = (item) => (toString.call(item) !== '[object Object]' ? item
+    : `{${getRenderResult(diff(item, item), depthСount + 1)}\n    ${space.repeat(depthСount)}}`);
+
+  if (status === 'added') return `${acc}\n${space.repeat(depthСount)}  + ${key}: ${getValue(val)}`;
+  if (status === 'deleted') return `${acc}\n${space.repeat(depthСount)}  - ${key}: ${getValue(val)}`;
+  if (status === 'unchanged') return `${acc}\n${space.repeat(depthСount)}    ${key}: ${getValue(val)}`;
+  if (status === 'changed') return `${acc}\n${space.repeat(depthСount)}  - ${key}: ${getValue(val)}\n${space.repeat(depthСount)}  + ${key}: ${getValue(nextVal)}`;
+  return `${acc}\n${space.repeat(depthСount)}    ${key}: {${getRenderResult(getValue(val), depthСount + 1)}\n    ${space.repeat(depthСount)}}`;
+}, '');
 
 export default (before, after, format = '') => {
   const extName = getFileExtension(before);
@@ -34,5 +37,5 @@ export default (before, after, format = '') => {
   const aConfig = parse(extName, after);
   if (format === 'plain') return formatter(bConfig, aConfig);
   if (format === 'json') return JSON.stringify(bConfig, aConfig);
-  return `{${startRendering(diff(bConfig, aConfig))}\n}`;
+  return `{${getRenderResult(diff(bConfig, aConfig))}\n}`;
 };
