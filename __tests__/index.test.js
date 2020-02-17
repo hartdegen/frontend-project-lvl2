a60e83755ddd317acd549766d322318def935505
hartdegen <hartdegen182@gmail.com>
2020-02-17 17:39:28 +0300

    tests refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,25 +2,34 @@ import fs from 'fs';
 import path from 'path';
 import diff from '../src';
 
-const pathToTestsDir = path.resolve(`${__dirname}/fixtures`);
-const resultJson = fs.readFileSync(path.resolve(`${pathToTestsDir}/resultJson`), 'utf-8');
-const resultBigIni = fs.readFileSync(path.resolve(`${pathToTestsDir}/resultBigIni`), 'utf-8');
-const resultBigYamlPlain = fs.readFileSync(path.resolve(`${pathToTestsDir}/resultBigYamlPlain`), 'utf-8');
-const exampleJson = fs.readFileSync(path.resolve(`${pathToTestsDir}/example.json`), 'utf-8');
+const pathToFile = (fileName) => path.resolve(`${__dirname}/fixtures/${fileName}`);
 
+const beforeJson = pathToFile('before.json');
+const afterJson = pathToFile('after.json');
+const resultJson = fs.readFileSync(pathToFile('resultJson'), 'utf-8');
+
+const bigBeforeIni = pathToFile('bigBefore.ini');
+const bigAfterIni = pathToFile('bigAfter.ini');
+const resultBigIni = fs.readFileSync(pathToFile('resultBigIni'), 'utf-8');
+
+const bigBeforeYaml = pathToFile('bigBefore.yaml');
+const bigAfterYaml = pathToFile('bigAfter.yaml');
+const resultBigYamlPlain = fs.readFileSync(pathToFile('resultBigYamlPlain'), 'utf-8');
+
+const bigBeforeJson = pathToFile('bigBefore.json');
+const bigAfterJson = pathToFile('bigAfter.json');
+const exampleJson = fs.readFileSync(pathToFile('example.json'), 'utf-8');
 
 
 test('check json diff', () => {
-  expect(diff(`${pathToTestsDir}/before.json`, `${pathToTestsDir}/after.json`)).toBe(resultJson);
+  expect(diff(beforeJson, afterJson)).toBe(resultJson);
 });
 test('check bigIni diff', () => {
-  expect(diff(`${pathToTestsDir}/bigBefore.ini`, `${pathToTestsDir}/bigAfter.ini`)).toBe(resultBigIni);
+  expect(diff(bigBeforeIni, bigAfterIni)).toBe(resultBigIni);
 });
 test('check bigYaml diff in "--format plain" mode', () => {
-  expect(diff(`${pathToTestsDir}/bigBefore.yaml`, `${pathToTestsDir}/bigAfter.yaml`, 'plain'))
-    .toBe(resultBigYamlPlain);
+  expect(diff(bigBeforeYaml, bigAfterYaml, 'plain')).toBe(resultBigYamlPlain);
 });
 test('check JSON converter', () => {
-  expect(diff(`${pathToTestsDir}/bigBefore.json`, `${pathToTestsDir}/bigAfter.json`, 'json'))
-    .toBe(exampleJson);
+  expect(diff(bigBeforeJson, bigAfterJson, 'json')).toBe(exampleJson);
 });
