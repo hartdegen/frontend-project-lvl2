2e2cd500d6220887bb35c496036d436a74ec2d2b
hartdegen <hartdegen182@gmail.com>
2020-02-17 18:34:40 +0300

    index.js refactoring

--- src/formatters/formatter.js
+++ src/formatters/formatter.js
@@ -1,5 +1,6 @@
 const keysOf = (obj) => Object.keys(obj);
 const valueOf = (obj) => (typeof obj !== 'object' ? obj : '[complex value]');
+const markForText = (item) => (typeof item === 'string' ? `"${item}"` : item);
 
 const makeFormatting = (b, a, name = '', keyName = '') => {
   const result = Object.entries({ ...b, ...a }).reduce((acc, value) => {
@@ -8,8 +9,8 @@ const makeFormatting = (b, a, name = '', keyName = '') => {
     if (typeof b[key] === 'object' && typeof a[key] === 'object' && b[key] !== a[key]) return [...acc, makeFormatting(b[key], a[key], name, `${keyName}.${key}`)];
     if (b[key] === a[key]) return [...acc];
     if (keysOf(b).includes(key) && !keysOf(a).includes(key)) return [...acc, `Property '${pathToVerifiableKey}' was deleted `];
-    return !keysOf(b).includes(key) && keysOf(a).includes(key) ? [...acc, `Property '${pathToVerifiableKey}' was added with value: '${valueOf(a[key])}' `]
-      : [...acc, `Property '${pathToVerifiableKey}' was changed from '${valueOf(b[key])}' to '${valueOf(a[key])}' `];
+    return !keysOf(b).includes(key) && keysOf(a).includes(key) ? [...acc, `Property '${pathToVerifiableKey}' was added with value: ${markForText(valueOf(a[key]))} `]
+      : [...acc, `Property '${pathToVerifiableKey}' was changed from ${markForText(valueOf(b[key]))} to ${markForText(valueOf(a[key]))} `];
   }, []);
   return result.flat(Infinity).join('\n');
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import parse from './parsers';
 import formatter from './formatters/formatter';
 
 const getFileExtension = (pathToFile) => path.extname(`${pathToFile}`);
-
 const keysOf = (obj) => Object.keys(obj);
 // b is "before"
 // a is "after"
@@ -24,11 +23,22 @@ const getRenderResult = (arr, depthСount = 0, space = '    ') => arr.reduce((ac
   const getValue = (item) => (toString.call(item) !== '[object Object]' ? item
     : `{${getRenderResult(diff(item, item), depthСount + 1)}\n    ${space.repeat(depthСount)}}`);
 
-  if (status === 'added') return `${acc}\n${space.repeat(depthСount)}  + ${key}: ${getValue(val)}`;
-  if (status === 'deleted') return `${acc}\n${space.repeat(depthСount)}  - ${key}: ${getValue(val)}`;
-  if (status === 'unchanged') return `${acc}\n${space.repeat(depthСount)}    ${key}: ${getValue(val)}`;
-  if (status === 'changed') return `${acc}\n${space.repeat(depthСount)}  - ${key}: ${getValue(val)}\n${space.repeat(depthСount)}  + ${key}: ${getValue(nextVal)}`;
-  return `${acc}\n${space.repeat(depthСount)}    ${key}: {${getRenderResult(getValue(val), depthСount + 1)}\n    ${space.repeat(depthСount)}}`;
+  switch (status) {
+    case 'added':
+      return `${acc}\n${space.repeat(depthСount)}  + ${key}: ${getValue(val)}`;
+
+    case 'deleted':
+      return `${acc}\n${space.repeat(depthСount)}  - ${key}: ${getValue(val)}`;
+
+    case 'unchanged':
+      return `${acc}\n${space.repeat(depthСount)}    ${key}: ${getValue(val)}`;
+
+    case 'changed':
+      return `${acc}\n${space.repeat(depthСount)}  - ${key}: ${getValue(val)}\n${space.repeat(depthСount)}  + ${key}: ${getValue(nextVal)}`;
+
+    default:
+      return `${acc}\n${space.repeat(depthСount)}    ${key}: {${getRenderResult(getValue(val), depthСount + 1)}\n    ${space.repeat(depthСount)}}`;
+  }
 }, '');
 
 export default (before, after, format = '') => {
