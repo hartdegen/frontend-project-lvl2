3264e60f519484ec702042b73490f3aa6b556356
hartdegen <hartdegen182@gmail.com>
2020-02-17 18:50:57 +0300

    index.js refactoring v.2

--- src/index.js
+++ src/index.js
@@ -11,10 +11,17 @@ const diff = (b, a = {}) => {
 
   return mergedConfigs.reduce((acc, value) => {
     const [key] = value;
-    if (b[key] === a[key]) return [...acc, ['unchanged', key, a[key]]];
-    if (typeof b[key] === 'object' && typeof a[key] === 'object') return [...acc, ['gottaCheckDeeper', key, diff(b[key], a[key])]];
-    if (keysOf(b).includes(key) && keysOf(a).includes(key)) return [...acc, ['changed', key, b[key], a[key]]];
-    return (!keysOf(b).includes(key)) ? [...acc, ['added', key, a[key]]] : [...acc, ['deleted', key, b[key]]];
+    if (b[key] === a[key]) {
+      return [...acc, ['unchanged', key, a[key]]];
+    }
+    if (typeof b[key] === 'object' && typeof a[key] === 'object') {
+      return [...acc, ['gottaCheckDeeper', key, diff(b[key], a[key])]];
+    }
+    if (keysOf(b).includes(key) && keysOf(a).includes(key)) {
+      return [...acc, ['changed', key, b[key], a[key]]];
+    }
+    return !keysOf(b).includes(key) ? [...acc, ['added', key, a[key]]]
+      : [...acc, ['deleted', key, b[key]]];
   }, []);
 };
 
@@ -42,10 +49,17 @@ const getRenderResult = (arr, depthСount = 0, space = '    ') => arr.reduce((ac
 }, '');
 
 export default (before, after, format = '') => {
-  const extName = getFileExtension(before);
-  const bConfig = parse(extName, before);
-  const aConfig = parse(extName, after);
-  if (format === 'plain') return formatter(bConfig, aConfig);
-  if (format === 'json') return JSON.stringify(bConfig, aConfig);
-  return `{${getRenderResult(diff(bConfig, aConfig))}\n}`;
+  const extName = (obj) => getFileExtension(obj);
+  const bConfig = parse(extName(before), before);
+  const aConfig = parse(extName(after), after);
+  switch (format) {
+    case 'plain':
+      return formatter(bConfig, aConfig);
+
+    case 'json':
+      return JSON.stringify(bConfig, aConfig);
+
+    default:
+      return `{${getRenderResult(diff(bConfig, aConfig))}\n}`;
+  }
 };
