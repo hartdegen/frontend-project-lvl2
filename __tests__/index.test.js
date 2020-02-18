e474fda27b677cf585c474d831877dde151d8afb
hartdegen <hartdegen182@gmail.com>
2020-02-18 16:13:19 +0300

    tests fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,32 +2,28 @@ import fs from 'fs';
 import path from 'path';
 import diff from '../src';
 
-const pathToFile = (fileName) => path.resolve(`${__dirname}/fixtures/${fileName}`);
+const pathTo = (fileName) => path.resolve(`${__dirname}/fixtures/${fileName}`);
+let resultTextFormat; let resultPlainFormat; let exampleJson;
 
-const beforeJson = pathToFile('before.json');
-const afterJson = pathToFile('after.json');
-const resultJsonFormat = fs.readFileSync(pathToFile('resultJsonFormat'), 'utf-8');
-
-const beforeIni = pathToFile('before.ini');
-const afterIni = pathToFile('after.ini');
-const resultIniFormat = fs.readFileSync(pathToFile('resultIniFormat'), 'utf-8');
-
-const beforeYaml = pathToFile('before.yaml');
-const afterYaml = pathToFile('after.yaml');
-const resultPlainYamlFormat = fs.readFileSync(pathToFile('resultPlainYamlFormat'), 'utf-8');
-
-const exampleJson = fs.readFileSync(pathToFile('example.json'), 'utf-8');
-
-
-test('check json diff', () => {
-  expect(diff(beforeIni, afterIni)).toBe(resultJsonFormat);
+beforeEach(() => {
+  resultTextFormat = fs.readFileSync(pathTo('resultTextFormat'), 'utf-8');
+  resultPlainFormat = fs.readFileSync(pathTo('resultPlainYamlFormat'), 'utf-8');
+  exampleJson = fs.readFileSync(pathTo('example.json'), 'utf-8');
 });
-test('check ini diff', () => {
-  expect(diff(beforeIni, afterIni)).toBe(resultIniFormat);
+
+
+test('check diff json', () => {
+  expect(diff(pathTo('before.json'), pathTo('after.json'))).toBe(resultTextFormat);
 });
-test('check yaml diff, "--format plain" mode', () => {
-  expect(diff(beforeYaml, afterYaml, 'plain')).toBe(resultPlainYamlFormat);
+test('check diff ini', () => {
+  expect(diff(pathTo('before.ini'), pathTo('after.ini'))).toBe(resultTextFormat);
+});
+test('check diff yaml', () => {
+  expect(diff(pathTo('before.yaml'), pathTo('after.yaml'))).toBe(resultTextFormat);
+});
+test('check diff yaml, "--format plain" mode', () => {
+  expect(diff(pathTo('before.yaml'), pathTo('after.yaml'), 'plain')).toBe(resultPlainFormat);
 });
 test('check json converter, "--format json" mode', () => {
-  expect(diff(beforeJson, afterJson, 'json')).toBe(exampleJson);
+  expect(diff(pathTo('before.yaml'), pathTo('after.yaml'), 'json')).toBe(exampleJson);
 });
