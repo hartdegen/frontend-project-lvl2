b566079cb012aa2c6798c7db0ca8a8b13e6faa25
hartdegen <hartdegen182@gmail.com>
2020-02-19 17:19:18 +0300

    tests refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,27 +3,16 @@ import path from 'path';
 import diff from '../src';
 
 const pathTo = (fileName) => path.resolve(`${__dirname}/fixtures/${fileName}`);
-let resultTextFormat; let resultPlainFormat; let exampleJson;
 
-beforeEach(() => {
-  resultTextFormat = fs.readFileSync(pathTo('resultTextFormat'), 'utf-8');
-  resultPlainFormat = fs.readFileSync(pathTo('resultPlainFormat'), 'utf-8');
-  exampleJson = fs.readFileSync(pathTo('example.json'), 'utf-8');
-});
-
-
-test('check diff json', () => {
-  expect(diff(pathTo('before.json'), pathTo('after.json'))).toBe(resultTextFormat);
-});
-test('check diff ini', () => {
-  expect(diff(pathTo('before.ini'), pathTo('after.ini'))).toBe(resultTextFormat);
-});
-test('check diff yaml', () => {
-  expect(diff(pathTo('before.yaml'), pathTo('after.yaml'))).toBe(resultTextFormat);
-});
-test('check diff yaml, "--format plain" mode', () => {
-  expect(diff(pathTo('before.yaml'), pathTo('after.yaml'), 'plain')).toBe(resultPlainFormat);
-});
-test('check json converter, "--format json" mode', () => {
-  expect(diff(pathTo('before.yaml'), pathTo('after.yaml'), 'json')).toBe(exampleJson);
+test.each([
+  ['before.json', 'after.json', '', 'resultTextFormat'],
+  ['before.ini', 'after.ini', '', 'resultTextFormat'],
+  ['before.yaml', 'after.yaml', '', 'resultTextFormat'],
+  ['before.yaml', 'after.yaml', 'plain', 'resultPlainFormat'],
+  ['before.yaml', 'after.yaml', 'json', 'example.json'],
+])('calculate differences between 2 configs', (before, after, format, resultFileName) => {
+  const b = pathTo(before);
+  const a = pathTo(after);
+  const expected = fs.readFileSync(pathTo(resultFileName), 'utf-8');
+  expect(diff(b, a, format)).toBe(expected);
 });
